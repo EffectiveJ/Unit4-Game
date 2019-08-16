4e5ac2182c88fba93c1506deb9c5a4bebbe912f3
EffectiveJ <jassaerivera@gmail.com>
2019-08-16 18:52:16 -0400

    images renamed, still not working

--- assets/js/javaScript.js
+++ assets/js/javaScript.js
@@ -1,53 +1,47 @@
 
 $(document).ready(function () {
-
     var characters = {
         "Naruto Uzumaki": {
             name: "Naruto Uzumaki",
             health: 120,
             attack: 12,
-            imageUrl: "../images/AnimeX_673432.jpeg",
+            imageUrl: "assets/images/ninja/naruto.jpeg",
             enemyAttackBack: 15
         },
         "Sasuke Uchiha": {
             name: "Sasuke Uchiha",
             health: 100,
             attack: 13,
-            imageUrl: "../images/AnimeX_606239.jpeg",
+            imageUrl: "assets/images/ninja/sasuke.jpeg",
             enemyAttackBack: 10
         },
         "Sakura Haruno": {
             name: "Sakura Haruno",
             health: 50,
             attack: 5,
-            imageUrl: "../images/AnimeX_644141.jpeg",
+            imageUrl: "assets/images/ninja/sakura.jpeg",
             enemyAttackBack: 5
         },
         "Kakashi Hatake": {
             name: "Kakashi Hatake",
             health: 175,
             attack: 20,
-            imageUrl: "../images/AnimeX_221968.jpeg",
+            imageUrl: "assets/images/ninja/kakashi.jpeg",
             enemyAttackBack: 20
         },
         "Madara Uchiha": {
             name: "Madara Uchiha",
             health: 180,
             attack: 25,
-            imageUrl: "../images/AnimeX_665330.jpeg",
+            imageUrl: "assets/images/ninja/madara.jpg",
             enemyAttackBack: 25
         }
     };
-
-
     var attacker;
     var cobatants = [],
         defnder,
         turnCounter = 1,
         killCount = 0;
-
-
-
     var renderCharacter = function (character, renderAera) {
         var charDiv = $("<div class = 'character' data name= '" + character.name + "'>");
         var charName = $("div class = 'character-name'>").text(character.name);
@@ -56,95 +50,73 @@ $(document).ready(function () {
         charDiv.append(charName).append(charImage).append(charHealth);
         $(renderAera).append(charDiv);
     };
-
-
-
     var initializeGame = function () {
         for (var key in characters) {
             renderCharacter(character[key], "#character-section");
         };
-
     };
     initializeGame();
-
-
-
     var updateCharacter = function (charObj, areaRender) {
         $(areaRender).empty();
-        renderCharacter(cahrObj, areaRender);
+        renderCharacter(charObj, areaRender);
     };
-
-
     var renderEnemies = function (enemyArr) {
         for (var i = 0; i < enemyArr.length; i++) {
             renderCharacter(enemyArr[i], "#available-to-attack-section");
         }
     };
-
     var renderMessage = function (message) {
         var gameMessageSet = $("#game-message");
         var newMessage = $("<div>").text(message);
         gameMessageSet.append(newMessage);
     };
-
     var restartGame = function (resultMessage) {
         var restart = $("<button>Restart</button>").click(function () {
             location.reload();
         });
-
         var gameState = $("<div>").text(resultMessage);
         $("body").append(gameState);
         $("body").append(restart);
     };
-
-
-
     var clearMessage = function () {
         var gameMessage = $("#game-message");
-
         gameMessage.text("");
     };
-
-
-
-    $("characters-section").on("click", ".character", function () {
-
+    $("#characters-section").on("click", ".character", function () {
         var name = $(this).attr("data-name");
-
+        if (!attacker) {
+            attacker = characters[name];
+            for (var key in characters) {
+                if (key !== name) {
+                    combatants.push(characters[key]);
+                }
+            }
+            $("#characters-section").hide();
+            updateCharacter(attacker, "#selected-character");
+            renderEnemies(combatants);
+        }
+    });
+    $("#available-to-attack-section").on("click", ".character", function () {
+        var name = $(this).attr("data-name");
         if ($("#defender").children().length === 0) {
             defender = characters[name];
             updateCharacter(defender, "#defender");
-
-
             $(this).remove();
             clearMessage();
         }
-
     });
-
-
     $("#attack-button").on("click", function () {
-
         if ($("defender").children().length === 0) {
             var attackMessage = "You attacked" + defender.name + "for" + attacker.attack * trunCounter + "damage.";
             var counterAttackMessage = defnder.name + "attacked you for" + defender.enemyAttackBack + "damage.";
             clearMessage();
-
-
-
             defender.health -= attacker.attack * turnCounter;
-
             if (defender.health > 0) {
                 updateCharacter(defender, "#defender");
                 renderMessage(attackMessage);
                 renderMessage(counterAttackMessage);
-
-
                 attacker.health -= defender.enemyAttackBack;
-
                 updateCharacter(attacker, "#selected-character");
-
-
                 if (attacker.health <= 0) {
                     clearMessage();
                     restartGame("You have been defeated......MATCH OVER!!!");
@@ -155,21 +127,18 @@ $(document).ready(function () {
                 $("#defender").empty();
                 var gameStateMessage = "You Have Defeated" + defender.name + ", You can now challenge another Shinnobi.";
                 renderMessage(gameStateMessage);
-
                 killCount++;
-
                 if (killCount >= cobatants.length) {
                     clearMessage();
                     $("#attack-button").off("click");
                     restartGame("YOU WIN!!...MATCH OVER!!");
                 }
             }
-
             turnCounter++;
         }
         else {
             clearMessage();
-            renderMessage("There is on enemy here");
+            renderMessage("There is no enemy here");
         }
     });
 });
